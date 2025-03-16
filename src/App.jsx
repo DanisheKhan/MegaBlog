1586db25bd257b4f48c8e0b6acde82bc4a53e588
Danish Khan <khandanishe125@gmail.com>
2025-03-16 09:57:44 +0530

    29. fine

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
 import { Footer, Header } from "./components";
 import { Outlet } from "react-router-dom";
-import { Bg } from "./components/Bg";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -26,7 +25,6 @@ function App() {
 
   return !loading ? (
     <>
-    {/* <Bg className=" -z-10 fixed top-0 left-0 " /> */}
       <div className="min-h-screen flex flex-col absolute  top-0 left-0 w-full h-full">
         <Header  />
         <main className="flex-grow mt-20">
--- src/components/Bg.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { WavyBackground } from "./ui/wavy-background";
-
-export function Bg({}) {
-  return <WavyBackground></WavyBackground>;
-}
-  
