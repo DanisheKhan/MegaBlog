d234611ee274f84b64e367e48af1335ff9f87a15
Danish Khan <khandanishe125@gmail.com>
2025-03-18 09:20:10 +0530

    45. fine

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -34,7 +34,7 @@ function Header() {
       <nav className="flex items-center justify-between h-16 container mx-auto px-4">
         <Link to="/" className="z-50">
         <div  className="scale-40">
-          {/* <MorphingTextDemo className="h-4" /> */}
+          <MorphingTextDemo className="h-4" />
         </div>
         </Link>
 
